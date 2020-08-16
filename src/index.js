e46b8a9afbd2dd13c9e4b2e70ed4fce8886ba2d5
Arka Das <arka19das@gmail.com>
2020-08-17 02:11:29 +0530

    Configuring React

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'; //first import bootstrap then css so that user defined css classes can override bootstrap css classes
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( < App / > , document.getElementById('root'));
 registerServiceWorker();
