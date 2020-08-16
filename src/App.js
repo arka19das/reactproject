45e998eb07b99788ead0d38715fc5a5079b89809
Arka Das <arka19das@gmail.com>
2020-08-17 04:17:26 +0530

    Component Part 2 W1c2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import Menu from './components/MenuComponent';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { DISHES } from './shared/dishes';
+
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
     render() {
 
         return ( <
-            div className = "App" >
+            div >
             <
             Navbar dark color = "primary" >
             <
@@ -17,8 +25,8 @@ class App extends Component {
             /NavbarBrand> < /
             div > <
             /Navbar> <
-            Menu / >
-            <
+            Menu dishes = { this.state.dishes }
+            / > <
             /div>
         );
     }
