9e605188db541adfe915b236431b5d9a91055464
Arka Das <arka19das@gmail.com>
2020-08-19 00:56:32 +0530

    SPA part 1 WEEk2 added

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,8 +3,12 @@ import Home from './HomeComponent.js';
 
 
 import Menu from './MenuComponent';
-
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
+import { LEADERS } from '../shared/leaders';
+
+import { PROMOTIONS } from '../shared/promotions';
+import { COMMENTS } from '../shared/comments';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -15,6 +19,10 @@ class Main extends Component {
         super(props);
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
+
             //selectedDish: null
         };
 
@@ -24,31 +32,48 @@ class Main extends Component {
 
     }
     render() {
-        console.log("main render component invoked");
-        const HomePage = () => {
-            return ( < Home / > );
-        }
-        return ( <div>
-            <Header / >
-            < Switch >
-            < Route path = '/home' component = { HomePage } /> 
-            <Route exact path = '/menu' component = {() => < Menu dishes = { this.state.dishes }/>} / >
-            <Redirect to = "/home" / >
-            </Switch>
-            <Footer / >
-            </div>
-            );
-        }
-    }
+            console.log("main render component invoked");
+            const HomePage = () => {
+                return ( < Home dish = { this.state.dishes.filter((dish) => dish.featured)[0] }
+                    promotion = { this.state.promotions.filter((promotion) => promotion.featured)[0] }
+                    leader = { this.state.leaders.filter((leader) => leader.featured)[0] }
+                    / > );
+                }
+                return ( < div >
+                    <
+                    Header / >
+                    <
+                    Switch >
+                    <
+                    Route path = '/home'
+                    component = { HomePage }
+                    />  <
+                    Route exact path = "/contactus"
+                    component = { Contact }
+                    /> <
+                    Route exact path = '/menu'
+                    component = {
+                        () => < Menu dishes = { this.state.dishes }
+                        />} / >
+                        <
+                        Redirect to = "/home" / >
+                        <
+                        /Switch> <
+                        Footer / >
+                        <
+                        /div>
+                    );
+                }
+            }
 
-    export default Main;
+            export default Main;
 
-    /*<
-                Menu dishes = { this.state.dishes }
-                onClick = {
-                    (dishId) => this.onDishSelect(dishId)
-                } //see here i have done this.state..... not this.prop.dishes
-                / >  <
-                DishDetail dish = { this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0] }
-                /> 
-                */
+            /*<
+                        Menu dishes = { this.state.dishes }
+                        onClick = {
+                            (dishId) => this.onDishSelect(dishId)
+                        } //see here i have done this.state..... not this.prop.dishes
+                        / >  <
+                        DishDetail dish = { this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0] }
+                        /> 
+                        */
